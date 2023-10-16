74222f1bd210c2e868679c87aab1e8735d927cfe
okokoren <okokoren@gmail.com>
2023-10-16 11:25:12 +0300

    feat(): added main container

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1>Home page</h1>
+  <b-container fluid>
+    <b-row align-h="center">
+      <b-col cols="4">
+        <h1>test</h1>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
